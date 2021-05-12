dca42f712f0b203af24234730f20fdbb3d10526a
Valery Yurchenko <36301615+vyurchenko1986@users.noreply.github.com>
2021-05-12 15:12:01 +0300

    Update config.sh

--- resources/config.sh
+++ resources/config.sh
@@ -1,1 +1,5 @@
-#!/bin/sh
+# Settings
+domain_name=ip_address          # hostname, ip_address or a custom value
+system_username=admin           # default username admin
+system_password=random          # random or a custom value
+system_branch=master            # master, stable
