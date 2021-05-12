43941aa47b894c538916dd20108d7dddecd03503
Valery Yurchenko <36301615+vyurchenko1986@users.noreply.github.com>
2021-05-12 15:27:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,31 @@
 #!/bin/sh
+
+#move to script directory so all relative paths work
+cd "$(dirname "$0")"
+
+#includes
+. ./resources/config.sh
+. ./resources/colors.sh
+. ./resources/environment.sh
+
+# removes the cd img from the /etc/apt/sources.list file (not needed after base install)
+sed -i '/cdrom:/d' /etc/apt/sources.list
+
+#Update to latest packages
+verbose "Update installed packages"
+apt-get update && apt-get upgrade -y
+
+#Add dependencies
+apt-get install -y wget
+apt-get install -y lsb-release
+apt-get install -y systemd
+apt-get install -y systemd-sysv
+apt-get install -y ca-certificates
+apt-get install -y dialog
+apt-get install -y nano
+apt-get install -y net-tools
+
+#SNMP
+apt-get install -y snmpd
+echo "rocommunity public" > /etc/snmp/snmpd.conf
+service snmpd restart
